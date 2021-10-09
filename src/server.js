b71b0de0d7535d06a540c4595fbce4e7a86d7b1b
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-10-09 15:42:03 +0100

    updates,error is now fixed

--- src/server.js
+++ src/server.js
@@ -28,15 +28,15 @@ server.use(genericErrorHandler) */
 
 mongoose.connect(process.env.MONGO_CONNECTION)
 
-mongoose.connection.on("error", err => {
-    console.log(err)
-  console.log("Successfully connected to Mongo!")
-  server.listen(port, () => {
-    console.table(listEndpoints(server))
-    console.log(`Server running on port ${port}`)
-  })
+
+mongoose.connection.on("connected", () => {
+    console.log("Successfully connected to Mongo!")
+    server.listen(port, () => {
+        console.table(listEndpoints(server))
+        console.log(`Server running on port ${port}`)
+    })
 })
 
 mongoose.connection.on("error", err => {
-  console.log(err)
+    console.log(err)
 })
